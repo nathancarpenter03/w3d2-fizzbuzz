67d1282e874619b96f5ea93839f85aef21513419
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-02-14 14:48:28 -0500

    added fizzbuzz

---
+++ flow-control.js
@@ -0,0 +1,75 @@
+// var todayIs = 'Tuesday';
+
+// if (todayIs === 'Tuesday') {
+//     console.log('Happy v day')
+// } else if (todayIs === 'Wednesday') {
+//     console.log('Happy Humpday!')
+// } else {
+//     console.log ('Happy' + todayIs + '!');
+// }
+
+// var todayIsTuesday = true;
+
+// console.log(typeOf todayIsTrue);
+// wouldr return Boolean
+
+// var counter = 0;
+// counter++;
+// counter = counter+2;
+// counter += 2;
+// console.log(counter);
+
+// if (counter > 2) {
+//     console.log('Correct Answer.');
+// }
+
+// Checking for divisibility using modular math
+// var sliecsOfPizza = 15;
+
+// if (slicesOfPizza % 3 === 0 && slicesOfPizza %5 === 0) {
+//     console.log('There\'s not enough for both of you.')
+// } else {
+//     console.log('more than enough.')
+// }
+
+// SWITCH STATEMENT
+
+// switch (temperature) {
+//     case 50;
+//         console.log('getting warmer');
+//         break;
+//     default: 
+//         console.log('who knows');
+// }
+
+// TURNARY OPERATORS
+
+// variableName ? 'do this' : 'else this'
+
+// var favoriteColor = 'blue';
+
+// var message = favoriteColor === 'red' ? 'My favorite color is red' : 'my fav color aint red.';
+
+// console.log(message);
+
+// FOR LOOPS
+
+// for (var i = 0; i < 500; i++) {
+//     console.log(i);
+// }
+
+
+// for (var i = 0; i <20; i++) {
+//     document.getElementById('robots').innerHTML += `<img src="http://robohash.org/indy${i}"/>`;
+// }
+
+// FUNCTIONS
+
+ /* function addOne (number) {
+    return number + 1;
+}
+
+var result = addone(100);
+
+console.log(result); */
+
